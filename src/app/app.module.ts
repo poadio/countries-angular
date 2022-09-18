4c34e1b77cbfd4cd10424ef3ef585f32fb5d1a2b
Stéphane ZUCATTI <stephane.zucatti@me.com>
2022-09-18 20:38:47 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Home } from './home/home.component';
 
 @NgModule({
   declarations: [AppComponent, Home],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, CommonModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
