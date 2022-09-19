879b6a9de943e5788907bc558876817a54b13e30
Stéphane ZUCATTI <stephane.zucatti@me.com>
2022-09-19 14:21:08 +0200

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,5 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  omneedia_url: 'https://tasty-walrus-30.demo.omneedia.net',
-  omneedia_key:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY2MzE3NjM1Mn0.Rjx5ICgMfDvZGZFvRS6H-73j7rNbozYZW8AMclQRdP0',
   production: false,
+  omneedia_url: '%OMNEEDIA_URL%',
+  omneedia_key: '%OMNEEDIA_KEY%',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
